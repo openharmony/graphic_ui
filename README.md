f1ee3e5ee82fb39dc191a069d6156ff03005c7c6
mamingshuai <mamingshuai1@huawei.com>
2021-03-11 18:38:06 +0800

    update openharmony 1.0.1

--- README.md
+++ README.md
@@ -1,39 +1,124 @@
-# graphic_ui
+# UI<a name="EN-US_TOPIC_0000001123180295"></a>
 
-#### 介绍
-{**以下是 Gitee 平台说明，您可以替换此简介**
-Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用 Gitee 实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+-   [Introduction](#section11660541593)
+-   [Directory Structure](#section161941989596)
+-   [Constraints](#section119744591305)
+-   [Compilation and Building](#section137768191623)
+-   [Description](#section1312121216216)
+    -   [Component Description](#section66002422015)
+    -   [Usage](#section129654513264)
 
-#### 软件架构
-软件架构说明
+-   [Repositories Involved](#section1371113476307)
+
+## Introduction<a name="section11660541593"></a>
+
+The graphics UI module implements a system-level graphics engine.
+
+This module provides the UIKit APIs for application development. You can use the APIs to add animations, manage layouts, transform images, process events, and operates on rich UI components.
+
+The graphics UI directly calls the HAL API or uses the client provided by the Window Manager Service \(WMS\) to interact with the hardware to complete operations such as event response and image drawing.
+
+**Figure  1**  Graphics subsystem architecture<a name="fig1358754417214"></a>  
+![](figures/graphics-subsystem-architecture.png "graphics-subsystem-architecture")
+
+## Directory Structure<a name="section161941989596"></a>
+
+```
+/foundation/graphic/ui
+├── frameworks                  # Framework code
+│   ├── animator                # Animator module
+│   ├── common                  # Common module
+│   ├── components              # Components
+│   ├── core                    # UI main processes (such as rendering and task management)
+│   ├── default_resource
+│   ├── dfx                     # Maintenance and test
+│   ├── dock                    # Driver adaptation layer
+│   │   └── ohos                # OHOS platform adaptation
+│   ├── draw                    # Drawing logic
+│   ├── engines                 # Drawing engines
+│   │   ├── dfb
+│   │   ├── general
+│   │   ├── gpu_vglite
+│   │   └── software_zlite
+│   ├── events                  # Events
+│   ├── font                    # Fonts
+│   ├── imgdecode               # Image management
+│   ├── layout                  # Page layout
+│   ├── themes                  # Theme management
+│   ├── window                  # Window management adaptation layer
+│   └── window_manager
+│       └── dfb
+├── interfaces                  # APIs
+│   ├── innerkits               # APIs between modules
+│   │   └── xxx                 # Sub-module APIs
+│   └── kits                    # Externel APIs
+│   │   └── xxx                 # Sub-module APIs
+├── test                        # Test code
+│   ├── framework
+│   │   ├── include             # Header files for the test framework
+│   │   └── src                 # Source code for the test framework
+│   ├── uitest                  # Display effect test (The executable program is in foundation/graphic/wms/test:sample_ui.)
+│   │   └── test_xxx            # Specific UI effect test
+│   └── unittest                # Unit testing
+│       └── xxx                 # Unit testing for a specific UI component
+└── tools                       # Test and simulation tools (simulator projects and resource files)
+    └── qt                      # Qt project
+```
+
+## Constraints<a name="section119744591305"></a>
+
+Platform Constraints
+
+-   The Windows platform supports only Qt and OHOS IDE.
+-   For the support of other platforms, see the  **graphic**  tag in  **vender/hisilicon/\[product\_name\]/config.json**. If the  **graphic**  tag does not exist, it indicates that the product does not have the graphics subsystem.
+
+## Compilation and Building<a name="section137768191623"></a>
+
+```
+# Generate the libui.so file in the out directory of the product folder through GN compilation.
+hb build lite_ui
+
+# To compile the Qt library, see the Qt simulator project at graphic/ui/tools/qt/simulator/simulator.pro.
+```
+
+## Description<a name="section1312121216216"></a>
+
+### Component Description<a name="section66002422015"></a>
+
+Components are classified into basic components and container components.
+
+-   Basic components: Implement only a single function, such as  **Text**,  **Button**,  **Image**, and  **List**.
+-   Container components: Hold and combine child components to implement complex functions.
+
+**Figure  2**  Graphics subsystem components<a name="fig1594213196218"></a>  
+![](figures/graphics-subsystem-components.png "graphics-subsystem-components")
+
+### Usage<a name="section129654513264"></a>
+
+For details about how to use components and APIs of graphics, see the examples provided in  **foundation/graphic/ui/test/uitest**.
+
+-   The Qt project can be debugged in the Windows environment.
+
+    Project file path:
+
+    ```
+    graphic/ui/tools/qt/simulator/simulator.pro
+    ```
+
+-   For other debugging environments, you can run  **foundation/graphic/wms/test:sample\_ui**.
+
+    ```
+    hb build lite_wms -b debug
+    ```
+
+    After the building is successful, the executable program  **out/\[product\_name\]/dev\_tools/bin/sample\_ui**  is obtained. You can run the program in an environment to view the display effect of a specific component.
 
 
-#### 安装教程
+## Repositories Involved<a name="section1371113476307"></a>
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+/hmf/graphic/surface
 
-#### 使用说明
+/hmf/graphic/wms
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+/hmf/graphic/utils
 
-#### 参与贡献
-
-1.  Fork 本仓库
-2.  新建 Feat_xxx 分支
-3.  提交代码
-4.  新建 Pull Request
-
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
---
+++ test/uitest/test_rotate_input/ui_test_rotate_input.cpp
@@ -0,0 +1,361 @@
+﻿/*
+ * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ui_test_rotate_input.h"
+#include "common/screen.h"
+#include "components/ui_label.h"
+#include "components/ui_label_button.h"
+#include "cstdio"
+#include "dock/focus_manager.h"
+
+#if ENABLE_ROTATE_INPUT
+namespace OHOS {
+static int16_t g_blank = 20;
+static int16_t g_listW = 200;
+static int16_t g_listH = 400;
+static int16_t g_scrollH = 200;
+static int16_t g_scrollW = 400;
+static int16_t g_buttonH = 500;
+static int16_t g_buttonW = 500;
+static int16_t g_smallButtonH = 80;
+static int16_t g_smallButtonW = 200;
+static int16_t g_swipeH = 200;
+static int16_t g_swipeW = 400;
+static int16_t g_sliderW = 40;
+static int16_t g_sliderH = 300;
+static const char* g_pickerRange[] = { "A0", "B1", "C2", "D3", "E4", "F5", "G6", "H7", "I8", "J9", "K10", "L11" };
+
+#if ENABLE_MOTOR
+void Print(MotorType motorType)
+{
+    switch (motorType) {
+        case MotorType::MOTOR_TYPE_ONE:
+            printf("MOTOR_TYPE_ONE\n");
+            break;
+        case MotorType::MOTOR_TYPE_TWO:
+            printf("MOTOR_TYPE_TWO\n");
+            break;
+        default:
+            break;
+    }
+}
+#endif
+
+void UITestRotateInput::SetUp()
+{
+    if (container_ == nullptr) {
+        container_ = new UIScrollView();
+        container_->SetOnTouchListener(this);
+        container_->Resize(Screen::GetInstance().GetWidth(), Screen::GetInstance().GetHeight());
+        container_->SetThrowDrag(true);
+        container_->SetRotateFactor(20); // 20 : rotate factor for container
+        container_->RequestFocus();
+        container_->SetReboundSize(50); // 50 : rebound size
+    }
+    if (adapterData_ == nullptr) {
+        adapterData_ = new List<const char*>();
+        adapterData_->PushBack("000");
+        adapterData_->PushBack("111");
+        adapterData_->PushBack("222");
+        adapterData_->PushBack("333");
+        adapterData_->PushBack("444");
+        adapterData_->PushBack("555");
+        adapterData_->PushBack("666");
+        adapterData_->PushBack("777");
+        adapterData_->PushBack("888");
+        adapterData_->PushBack("999");
+        adapterData_->PushBack("///");
+        adapterData_->PushBack("***");
+        adapterData_->PushBack("+++");
+        adapterData_->PushBack("---");
+        adapterData_->PushBack("...");
+        adapterData_->PushBack("@@@");
+        adapterData_->PushBack("000");
+        adapterData_->PushBack("111");
+        adapterData_->PushBack("222");
+        adapterData_->PushBack("333");
+        adapterData_->PushBack("444");
+        adapterData_->PushBack("555");
+        adapterData_->PushBack("666");
+        adapterData_->PushBack("777");
+        adapterData_->PushBack("888");
+        adapterData_->PushBack("999");
+        adapterData_->PushBack("///");
+        adapterData_->PushBack("***");
+        adapterData_->PushBack("+++");
+        adapterData_->PushBack("---");
+        adapterData_->PushBack("...");
+        adapterData_->PushBack("@@@");
+    }
+#if ENABLE_MOTOR
+    FocusManager::GetInstance()->RegisterMotorFunc(Print);
+#endif
+}
+
+void UITestRotateInput::TearDown()
+{
+    container_->ClearFocus();
+    container_->Remove(timePicker_);
+    container_->Remove(picker_);
+    DeleteChildren(container_);
+    container_ = nullptr;
+    if (adapterData_ != nullptr) {
+        adapterData_->Clear();
+        delete adapterData_;
+        adapterData_ = nullptr;
+    }
+    if (adapter_ != nullptr) {
+        delete adapter_;
+        adapter_ = nullptr;
+    }
+    lastX_ = 0;
+    lastY_ = 0;
+    list_ = nullptr;
+    scroll_ = nullptr;
+    swipe_ = nullptr;
+    timePicker_ = nullptr;
+    slider_ = nullptr;
+    picker_ = nullptr;
+}
+
+const UIView* UITestRotateInput::GetTestView()
+{
+    UIKit_Rotate_Event_List_001();
+    UIKit_Rotate_Event_Scroll_View_002();
+    UIKit_Rotate_Event_Swipe_View_003();
+    UIKit_Rotate_Event_Slider_004();
+    UIKit_Rotate_Event_Time_Picker_005();
+    UIKit_Rotate_Event_Picker_006();
+    return container_;
+}
+
+void UITestRotateInput::UIKit_Rotate_Event_List_001()
+{
+    if (container_ == nullptr) {
+        return;
+    }
+    UILabel* label = GetTitleLabel("UIList Test");
+    container_->Add(label);
+    label->SetPosition(0, lastY_ + g_blank);
+    if (adapter_ == nullptr) {
+        adapter_ = new TextAdapter();
+    }
+    adapter_->SetData(adapterData_);
+    if (list_ == nullptr) {
+        list_ = new UIList(UIList::VERTICAL);
+    }
+    list_->SetStyle(STYLE_BACKGROUND_COLOR, Color::Black().full);
+    list_->SetPosition(0, label->GetY() + g_blank, g_listW, g_listH - 200); // 200: mean list reduce height
+    list_->SetStartIndex(6);                                                // 6 : list start index
+    list_->SetAdapter(adapter_);
+    list_->SetOnTouchListener(this);
+    list_->SetThrowDrag(true);
+    list_->SetRotateFactor(10); // 10 : rotate factor
+    list_->SetReboundSize(50); // 50 : rebound size
+    container_->Add(list_);
+    SetLastPos(list_);
+}
+
+void UITestRotateInput::UIKit_Rotate_Event_Scroll_View_002()
+{
+    if (container_ == nullptr) {
+        return;
+    }
+    UILabel* label = GetTitleLabel("UIScrollView Test");
+    container_->Add(label);
+    label->SetPosition(0, lastY_ + g_blank);
+    if (scroll_ == nullptr) {
+        scroll_ = new UIScrollView();
+    }
+    scroll_->SetStyle(STYLE_BACKGROUND_COLOR, Color::Black().full);
+    scroll_->SetPosition(0, label->GetY() + g_blank, g_scrollW, g_scrollH);
+    scroll_->SetXScrollBarVisible(true);
+    scroll_->SetYScrollBarVisible(true);
+    scroll_->SetRotateFactor(10); // 10 : rotateFactor
+    UIButton* btn = new UIButton();
+    btn->SetPosition(0, 0, g_buttonW, g_buttonH);
+    scroll_->Add(btn);
+    scroll_->SetThrowDrag(true);
+    scroll_->SetOnTouchListener(this);
+    scroll_->SetReboundSize(50); // 50 : rebound size
+    SetLastPos(scroll_);
+    container_->Add(scroll_);
+}
+
+void UITestRotateInput::UIKit_Rotate_Event_Swipe_View_003()
+{
+    if (container_ == nullptr) {
+        return;
+    }
+    UILabel* label = GetTitleLabel("UISwipeView Test");
+    container_->Add(label);
+    label->SetPosition(0, lastY_ + g_blank);
+    if (swipe_ == nullptr) {
+        swipe_ = new UISwipeView(UISwipeView::HORIZONTAL);
+    }
+    swipe_->SetStyle(STYLE_BACKGROUND_COLOR, Color::Black().full);
+    swipe_->SetPosition(0, label->GetY() + g_blank, g_swipeW, g_swipeH);
+    swipe_->SetBlankSize(100); // 100 is blank size
+    UILabelButton* btn1 = new UILabelButton();
+    UILabelButton* btn2 = new UILabelButton();
+    UILabelButton* btn3 = new UILabelButton();
+    UILabelButton* btn4 = new UILabelButton();
+    UILabelButton* btn5 = new UILabelButton();
+    UILabelButton* btn6 = new UILabelButton();
+    UILabelButton* btn7 = new UILabelButton();
+    btn1->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn2->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn3->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn4->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn5->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn6->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn7->SetPosition(0, 0, g_smallButtonW, g_smallButtonH);
+    btn1->SetText("Button 1");
+    btn2->SetText("Button 2");
+    btn3->SetText("Button 3");
+    btn4->SetText("Button 4");
+    btn5->SetText("Button 5");
+    btn6->SetText("Button 6");
+    btn7->SetText("Button 7");
+    swipe_->Add(btn1);
+    swipe_->Add(btn2);
+    swipe_->Add(btn3);
+    swipe_->Add(btn4);
+    swipe_->Add(btn5);
+    swipe_->Add(btn6);
+    swipe_->Add(btn7);
+    swipe_->SetOnTouchListener(this);
+    swipe_->SetRotateFactor(30); // 30 : rotate factor
+    container_->Add(swipe_);
+    SetLastPos(swipe_);
+}
+
+void UITestRotateInput::UIKit_Rotate_Event_Slider_004()
+{
+    if (container_ == nullptr) {
+        return;
+    }
+    UILabel* label = GetTitleLabel("UISlider Test");
+    container_->Add(label);
+    label->SetPosition(0, lastY_ + g_blank);
+    if (slider_ == nullptr) {
+        slider_ = new UISlider();
+    }
+    slider_->SetPosition(50, label->GetY() + g_blank, g_sliderW, g_sliderH); // 50: position x
+    slider_->SetValidHeight(200);                                            // 200: valid height
+    slider_->SetValue(0);                                                    // 0: progress bar current value
+    slider_->SetDirection(UISlider::Direction::DIR_BOTTOM_TO_TOP);
+    slider_->SetStyle(STYLE_BACKGROUND_COLOR, Color::Gray().full);
+    slider_->SetOnTouchListener(this);
+    container_->Add(slider_);
+    SetLastPos(slider_);
+}
+void UITestRotateInput::UIKit_Rotate_Event_Time_Picker_005()
+{
+    if (container_ == nullptr) {
+        return;
+    }
+    UILabel* label = GetTitleLabel("UITimePicker Test");
+    container_->Add(label);
+    label->SetPosition(0, lastY_ + g_blank);
+    if (timePicker_ == nullptr) {
+        timePicker_ = new UITimePicker();
+    }
+    timePicker_->EnableSecond(true);
+    timePicker_->SetPosition(0, label->GetY() + g_blank, g_listW, g_listH);
+    timePicker_->SetItemHeight(50); // 50 height
+
+    UIView* hour = timePicker_->GetChildById("hour");
+    if (hour == nullptr) {
+        return;
+    }
+    hour->SetOnTouchListener(this);
+
+    UIView* minute = timePicker_->GetChildById("minute");
+    if (minute == nullptr) {
+        return;
+    }
+    minute->SetOnTouchListener(this);
+
+    UIView* second = timePicker_->GetChildById("second");
+    if (second == nullptr) {
+        return;
+    }
+    second->SetOnTouchListener(this);
+
+    container_->Add(timePicker_);
+    SetLastPos(timePicker_);
+}
+
+void UITestRotateInput::UIKit_Rotate_Event_Picker_006()
+{
+    if (container_ == nullptr) {
+        return;
+    }
+    UILabel* label = GetTitleLabel("UIPicker Test");
+    container_->Add(label);
+    label->SetPosition(0, lastY_ + g_blank);
+    if (picker_ == nullptr) {
+        picker_ = new UIPicker();
+    }
+    picker_->SetPosition(0, label->GetY() + g_blank, g_listW, g_listH);
+    picker_->SetItemHeight(50); // 50: height
+    picker_->SetValues(g_pickerRange, sizeof(g_pickerRange) / sizeof(g_pickerRange[0]));
+    picker_->SetOnTouchListener(this);
+    container_->Add(picker_);
+    SetLastPos(picker_);
+}
+bool UITestRotateInput::OnPress(UIView& view, const PressEvent& event)
+{
+    if (&view == container_) {
+        container_->RequestFocus();
+        return true;
+    } else if (&view == list_) {
+        list_->RequestFocus();
+        return true;
+    } else if (&view == scroll_) {
+        scroll_->RequestFocus();
+        return true;
+    } else if (&view == swipe_) {
+        swipe_->RequestFocus();
+        return true;
+    } else if (&view == slider_) {
+        slider_->RequestFocus();
+        return true;
+    } else if (&view == timePicker_->GetChildById(UITimePicker::HOUR_LIST_NAME)) {
+        view.RequestFocus();
+        return true;
+    } else if (&view == timePicker_->GetChildById(UITimePicker::MIN_LIST_NAME)) {
+        view.RequestFocus();
+        return true;
+    } else if (&view == timePicker_->GetChildById(UITimePicker::SEC_LIST_NAME)) {
+        view.RequestFocus();
+        return true;
+    } else if (&view == picker_) {
+        picker_->RequestFocus();
+        return true;
+    }
+    return false;
+}
+void UITestRotateInput::SetLastPos(UIView* view)
+{
+    if (view == nullptr) {
+        return;
+    }
+    lastX_ = view->GetX();
+    lastY_ = view->GetY() + view->GetHeight();
+}
+} // namespace OHOS
+#endif
