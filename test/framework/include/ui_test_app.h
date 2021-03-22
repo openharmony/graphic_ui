e5fd81b8e5868a2dc5e7de0e64271843d3ad8845
zhdengc <15223574545@163.com>
2021-03-22 18:32:12 +0800

    add auto test framework

--- test/framework/include/ui_test_app.h
+++ test/framework/include/ui_test_app.h
@@ -22,7 +22,7 @@
 #include "components/ui_list.h"
 #include "test_case_list_adapter.h"
 
-#define OHOS_GRAPHIC_UI_AUTO_TEST
+#define OHOS_GRAPHIC_UI_AUTO_TEST 0
 namespace OHOS {
     namespace {
         constexpr char* UI_TEST_MAIN_LIST_ID = "main_list";
