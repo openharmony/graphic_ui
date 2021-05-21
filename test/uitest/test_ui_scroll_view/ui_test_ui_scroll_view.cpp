f5a233310d2bc2f707f495e6cd7675ea814b8ab0
guyuanzhang <zhangguyuan@huawei.com>
2021-05-21 15:54:56 +0800

    Description:fix scroll view rebound Sig:graphic Feature or Bugfix:Feature Binary Source:No

--- test/uitest/test_ui_scroll_view/ui_test_ui_scroll_view.cpp
+++ test/uitest/test_ui_scroll_view/ui_test_ui_scroll_view.cpp
@@ -112,6 +112,7 @@ void UITestUIScrollView::UIKit_UIScrollView_Test_bar_001()
     scroll->SetPosition(positionX_, positionY_, g_scrollW, g_scrollH);
     scroll->SetXScrollBarVisible(false);
     scroll->SetYScrollBarVisible(false);
+    scroll->SetReboundSize(50);
     container_->Add(scroll);
     UILabelButton* button1 = new UILabelButton();
     button1->SetPosition(0, 0, g_buttonW, g_buttonH);
